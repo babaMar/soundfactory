3419fb451de188898c3043942f79f88bb62b4416
labaffa <dellarovere.lante@gmail.com>
2019-09-03 17:57:41 +0200

    Add functions to build signal by means of numpy.fft.ifft

--- utils/signal.py
+++ utils/signal.py
@@ -37,3 +37,55 @@ def sf_wav_sub(depth, default=16):
         print("depth not available, default {} bits selected".format(default))
         r = "PCM_{}".format(default)
     return r
+
+
+def freq_indexes(freqs, n=44100, samplerate=44100):
+    """
+    Find positions of freqs in the numpy.fft.fftfreq
+    with given n e spacing = 1 / samplerate
+
+    Params
+    ------
+    freqs (float or array) -- frequencies to find index of
+    n (int) -- the n argument in numpy.fft.fftfreq
+    samplerate (float) -- points per second
+
+    Returns
+    -------
+    array of indexes
+
+    """
+    d = 1 / samplerate
+    return np.round(freqs * n * d).astype(int)
+
+
+def build_fft(freqs, amps, phases, period=1, samplerate=44100):
+    """
+    Build fft 'standard' components (see numpy.fft docs) from
+    given freqs, amps, phases, and a signal with given
+    period and samplerate
+
+    NOTE:
+    freqs, amps and phases must be in the same order to refer to the
+    Fourier component
+    """
+    idx = np.where(freqs >= 0)[0]
+    freqs, amps, phases = freqs[idx], amps[idx], phases[idx]
+    n = np.round(period * samplerate).astype(int)
+    amps = amps * n
+
+    N = (n-1)//2 + 1
+    r = np.zeros(N, dtype=np.complex_)
+    freq_idx = freq_indexes(freqs, n=n, samplerate=samplerate)
+    pos_X_k = amps * np.exp(1j * phases)
+    r[freq_idx] = pos_X_k
+    if N > n//2:
+        neg_X_k = r[::-1][:-1].conj()
+    else:
+        neg_X_k = np.concatenate(([0], r[::-1][:-1])).conj()
+    return np.concatenate((r, neg_X_k))
+
+
+def build_signal(freqs, amps, phases, period=1, samplerate=44100):
+    fft = build_fft(freqs, amps, phases, period=period, samplerate=samplerate)
+    return np.fft.ifft(fft)
