164dfaf37b6c4f767afa20662c0e62d9a5e4a6a5
labaffa <dellarovere.lante@gmail.com>
2019-09-04 17:37:55 +0200

    Add phases argument and input_checker

--- classes/signal_builder.py
+++ classes/signal_builder.py
@@ -2,15 +2,23 @@ import numpy as np
 from settings.signal import B_N_COEFF_MAP
 from utils.signal import wn, sf_wav_sub
 import soundfile as sf
+import numbers
 
 
 class SignalBuilder(object):
     """ Create a Signal from Fourier Series """
     def __init__(
-            self, frequencies, amplitudes,
-            n_max=1000, wave_type='square', t_resolution=10000):
-        self.amplitudes = set(amplitudes)
-        self.frequencies = set(frequencies)
+            self,
+            frequencies,
+            amplitudes,
+            phases=None,
+            n_max=1000,
+            wave_type='square',
+            t_resolution=10000):
+        self.frequencies = frequencies
+        self.amplitudes = amplitudes
+        self.set_phases(phases)
+        self.check_input()
         self.nterms = n_max
         supported_forms = list(B_N_COEFF_MAP.keys())
         if wave_type not in supported_forms:
@@ -49,3 +57,18 @@ class SignalBuilder(object):
     def export(self, filename, bit_depth=16, samplerate=44100):
         subtype = sf_wav_sub(bit_depth)
         sf.write(filename, self.signal, samplerate, subtype=subtype)
+
+    def check_input(self):
+        f, a, p = self.frequencies, self.amplitudes, self.phases
+        if not len(set(f)) == len(f):
+            raise ValueError("Same frequencies in the input")
+        if len({len(x) for x in [f, a, p]}) > 1:
+            raise ValueError("Inputs must have same lenght")
+        if any(not isinstance(x, numbers.Real) for l in [f, a, p] for x in l):
+            raise TypeError("Can use real numbers")
+
+    def set_phases(self, phases):
+        if phases is None:
+            self.phases = [0] * len(self.frequencies)
+        else:
+            self.phases = phases
