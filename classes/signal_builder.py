34c91821509ee4f5f6b9390711096858a5c25e84
Stefano Messina <stefano.messina@customergauge.com>
2019-09-06 12:16:59 +0200

    Raise base-class exceptions instead of general ones

--- classes/signal_builder.py
+++ classes/signal_builder.py
@@ -1,8 +1,42 @@
+import numbers
 import numpy as np
+import soundfile as sf
+
 from settings.signal import B_N_COEFF_MAP
 from utils.signal import wn, find_soundfile_subtype
-import soundfile as sf
-import numbers
+from settings.logging_settings import createlog
+
+
+class SignalBuilderError(Exception):
+    pass
+
+
+class NotSupportedWaveShape(SignalBuilderError):
+    def __init__(self, wave_shape, supported_shapes):
+        self.message = '{} wave_type not supported. It must be one of {}'\
+            .format(wave_shape, supported_shapes)
+        createlog.error(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class IdenticalFrequenciesDetected(SignalBuilderError):
+    def __init__(self):
+        self.message = 'Found duplicate frequency value in provided input'
+        createlog.error(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class ProvidedInputError(SignalBuilderError):
+    def __init__(self, message):
+        self.message = message
+        createlog.error(self.message)
+
+    def __str__(self):
+        return self.message
 
 
 class SignalBuilder(object):
@@ -17,13 +51,14 @@ class SignalBuilder(object):
             t_resolution=10000):
         self.frequencies = frequencies
         self.amplitudes = amplitudes
+        self.phases = None
         self.set_phases(phases)
         self.check_input()
         self.nterms = n_max
-        supported_forms = list(B_N_COEFF_MAP.keys())
-        if wave_type not in supported_forms:
-            raise Exception(
-                'Wave form type must be one of %s' % supported_forms)
+        supported_wave_shapes = list(B_N_COEFF_MAP.keys())
+        if wave_type not in supported_wave_shapes:
+            raise NotSupportedWaveShape(wave_type, supported_wave_shapes)
+
         self.time_resolution = t_resolution
         self.time_space = np.linspace(
             0., 1., self.time_resolution, endpoint=False)
@@ -61,11 +96,11 @@ class SignalBuilder(object):
     def check_input(self):
         f, a, p = self.frequencies, self.amplitudes, self.phases
         if not len(set(f)) == len(f):
-            raise ValueError("Same frequencies in the input")
+            raise IdenticalFrequenciesDetected()
         if len({len(x) for x in [f, a, p]}) > 1:
-            raise ValueError("Inputs must have same lenght")
+            raise ProvidedInputError('Provided frequency, amplitudes, and phases need to be equals in number')
         if any(not isinstance(x, numbers.Real) for l in [f, a, p] for x in l):
-            raise TypeError("Use just real numbers (floats or ints)")
+            raise ProvidedInputError('Use only real numbers (floats or ints)')
 
     def set_phases(self, phases):
         if phases is None:
