557c11c68f94770bf3268b62cce0be186b81d98c
labaffa <dellarovere.lante@gmail.com>
2019-09-06 11:35:00 +0200

    Add logging_settings

---
+++ settings/logging_settings.py
@@ -0,0 +1,7 @@
+from utils.helpers import get_logger
+import logging
+
+viewlog = get_logger(name="view")
+viewlog.setLevel(logging.INFO)
+createlog = get_logger(name="create")
+createlog.setLevel(logging.INFO)
