873c13edabce62db5ba1b1051213ebb52f40b94c
labaffa <dellarovere.lante@gmail.com>
2019-09-06 14:07:20 +0200

    Fix test_signal_builder

--- tests/test_signal_builder.py
+++ tests/test_signal_builder.py
@@ -7,9 +7,17 @@ from tests.conftest import (
     )
 import matplotlib.pyplot as plt
 
+
 TIME_RANGE = time_range()
-TEST_FREQUENCIES = [27.0, 54.0, 108.0, 216.0, 432.0, 864.0, 1728.0, 3456.0, 6912.0, 13824.0, 27648.0]
+TEST_FREQUENCIES = [
+    27.0, 54.0, 108.0, 216.0, 432.0, 864.0,
+    1728.0, 3456.0, 6912.0, 13824.0, 27648.0
+]
 APPROXIMATION_TOLERANCE = 0.01
+WAVE_ANALYTICS = [sine_wave, square_wave, sawtooth_wave, triangle_wave]
+WAVE_LABELS = ['sine', 'square', 'sawtooth', 'triangle']
+WAVE_TOLERANCES = [1e-12, 1e-3, 1e-3, 1e-12]
+WAVES = zip(WAVE_ANALYTICS, WAVE_LABELS, WAVE_TOLERANCES)
 
 
 class ApproximationDifferences:
@@ -21,51 +29,18 @@ class ApproximationDifferences:
 
 # Y: Diff (for each wave form) X: Freq
 def test_signal_approximation():
-    for freq in TEST_FREQUENCIES: # [choice(TEST_FREQUENCIES) for _ in range(4)]:
-        for analytic_sig, wave_shape in zip([sine_wave, square_wave, sawtooth_wave, triangle_wave],
-                                        ['sine', 'square', 'sawtooth', 'triangle']):
+    for freq in TEST_FREQUENCIES:
+        for analytic_sig, wave_shape, tolerance in WAVES:
             sig = analytic_sig(freq)
-            signal_builder = SignalBuilder([freq],
-                                           [1.],
-                                           n_max=200,
-                                           wave_type=wave_shape,
-                                           t_resolution=TIME_RANGE.size)
-            rec_sig = signal_builder.build_signal()
+            signal_builder = SignalBuilder(
+                [freq],
+                [1.],
+                n_max=200,
+                wave_type=wave_shape,
+                t_resolution=TIME_RANGE.size)
+            rec_sig = signal_builder.signal
             diff = rec_sig - sig
-            wave_shape_diff = getattr(ApproximationDifferences, wave_shape)
-            wave_shape_diff.append(abs(diff.mean()))
-
-            print(f'======= {wave_shape.upper()} =======', f'@ {freq} Hz')
-            print('Average difference between signals: ', diff.mean(), ' ',
-                  f'{diff[diff > APPROXIMATION_TOLERANCE].shape} Samples where difference > {APPROXIMATION_TOLERANCE}')
-            print()
-#            plt.figure()
-#            plt.suptitle(wave_shape.upper() + f' WAVE FORM: Built Signal - Analytic Signal @ {freq} Hz', y=1.)
-#
-#            plt.subplot(3, 1, 1)
-#            period = 2 * np.pi/freq
-#            indexes = np.where(TIME_RANGE <= period)
-#            for curve, label in zip([rec_sig, sig],
-#                                    ['Built Signal', 'Scipy Signal']):
-#                plt.plot(TIME_RANGE[indexes], curve[indexes], label=label)
-#            plt.legend(loc='best')
-#
-#            plt.subplot(3, 1, 2)
-#            plt.hist(diff, 100, histtype='step', density=True)
-#
-#            plt.subplot(3, 1, 3)
-#            plt.plot(TIME_RANGE[indexes], diff[indexes])
-#            plt.show()
-
-            # assert abs(diff.mean()) < APPROXIMATION_TOLERANCE * 0.1
-            # mean difference vs frequency?
-    plt.figure()
-    for wave_form in ['sine', 'square', 'sawtooth', 'triangle']:
-        plt.plot(TEST_FREQUENCIES, getattr(ApproximationDifferences, wave_form), label=wave_form.upper())
-    plt.legend(loc='best')
-
-
-test_signal_approximation()
+            assert diff.mean() < tolerance
 
 
 def test_export():
@@ -84,4 +59,3 @@ def test_export():
     idx = np.where((amps > 1e-4) & (freqs >= 0))[0]
     assert (freqs[idx] - f < 1e-5).all()
     assert (amps[idx] - a < 1e-5).all()
-
