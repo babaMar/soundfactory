01f98961134a59aeed9521a50cdbac70d1e3ba42
labaffa <dellarovere.lante@gmail.com>
2019-09-06 11:35:45 +0200

    Use global logger and remove prints

--- create.py
+++ create.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import click
+import numpy as np
 from classes.signal_builder import SignalBuilder
-from utils.helpers import get_logger
 from settings.input_validators import Wav
 from settings.signal import B_N_COEFF_MAP
-import logging
-import numpy as np
+from settings.logging_settings import createlog
+
+
+logger = createlog
 
 
 @click.command()
@@ -30,11 +32,8 @@ def main(freq_amp, wave, out, samplerate):
     save it on an out file
 
     """
-    logger = get_logger(__name__)
-    logger.setLevel(logging.INFO)
     freqs = [x[0] for x in freq_amp]
     amps = [x[1] for x in freq_amp]
-    print(freqs, amps)
     freqs, amps = np.array(freqs), np.array(amps)
     logger.info("Building signal")
     s = SignalBuilder(freqs, amps, wave_type=wave, t_resolution=samplerate)
