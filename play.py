d9cf1a930b861b103ca825a1e6bba1b8d7a37a4f
labaffa <dellarovere.lante@gmail.com>
2019-09-11 21:40:46 +0200

    Fix player for mono audios

--- play.py
+++ play.py
@@ -17,7 +17,8 @@ from utils.helpers import progress_time
 def main(input_file):
     with sf.SoundFile(input_file) as f:
         channels, samplerate = f.channels, f.samplerate
-        audio = f.read(dtype="int16")
+        audio = f.read(dtype="float32")
+        audio = (audio * 32767).astype("int16")
         total_seconds = len(f) / samplerate
         play_obj = sa.play_buffer(audio, channels, 2, samplerate)
         start_time = time.time()
