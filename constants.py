94299830de37a467af2e971ceab5d5a25ff1a9e2
labaffa <dellarovere.lante@gmail.com>
2019-09-16 19:12:25 +0200

    Nuances

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 from itertools import cycle
 
+
 BASE = 2
 CENTS_PER_OCTAVE = 1200
 CENT = pow(BASE, 1. / CENTS_PER_OCTAVE)
@@ -13,7 +14,9 @@ QUARTER_TONE_FLAT_SYMBOL = '𝄳'
 A_SUB_SUB_CONTRA_FREQ = 13.75
 _24_TET_SCALE_INIT = {'A-1': A_SUB_SUB_CONTRA_FREQ}
 
-TONE_LABELS = cycle(('B', 'B', 'C', 'D', 'D', 'E', 'E', 'F', 'G', 'G', 'A', 'A'))
+TONE_LABELS = cycle((
+    'B', 'B', 'C', 'D', 'D', 'E',
+    'E', 'F', 'G', 'G', 'A', 'A'))
 
 
 SEQUENCE_24 = (
