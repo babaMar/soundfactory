fb04e3cfb2956ba0bb9b98f958ceef31c31339dd
labaffa <dellarovere.lante@gmail.com>
2019-10-11 13:24:18 +0200

    Use DEFAULT_SAMPLERATE in conftest functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,34 +2,33 @@ import pytest
 import numpy as np
 import scipy.signal as signal
 from settings.input_validators import DEFAULT_WAVE_TYPE
-
-SAMPLES = 44100
-
-
-def time_range(start=0., end=1.):
-    return np.linspace(start, end, SAMPLES, endpoint=False)
+from constants import DEFAULT_SAMPLERATE
 
 
-def sine_wave(freq):
-    t = time_range()
+def time_range(start=0., end=1., samples=DEFAULT_SAMPLERATE):
+    return np.linspace(start, end, samples, endpoint=False)
+
+
+def sine_wave(freq, samples=DEFAULT_SAMPLERATE):
+    t = time_range(samples=samples)
     sig = np.sin(2 * np.pi * freq * t)
     return sig
 
 
-def square_wave(freq):
-    t = time_range()
+def square_wave(freq, samples=DEFAULT_SAMPLERATE):
+    t = time_range(samples=samples)
     sig = signal.square(2 * np.pi * freq * t)
     return sig
 
 
-def sawtooth_wave(freq):
-    t = time_range()
+def sawtooth_wave(freq, samples=DEFAULT_SAMPLERATE):
+    t = time_range(samples=samples)
     sig = signal.sawtooth(2 * np.pi * freq * t)
     return sig
 
 
-def triangle_wave(freq):
-    t = time_range()
+def triangle_wave(freq, samples=DEFAULT_SAMPLERATE):
+    t = time_range(samples=samples)
     phase_shift = np.pi / 2.  # a phase shift of 90° is needed to have signal=0. at t=0.
     sig = signal.sawtooth(2 * np.pi * freq * t + phase_shift, width=0.5)
     return sig
