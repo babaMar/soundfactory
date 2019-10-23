40dc4f8f18c8339e5f779c6c8cf9b874c1bab709
Stefano Messina <stefano.messina@customergauge.com>
2019-10-23 21:38:54 +0200

    Prepare for release

--- setup.py
+++ setup.py
@@ -11,19 +11,29 @@ with open('requirements.txt') as f:
     
 setup(
     name="soundfactory",
-    version="0.0.1",
-    author="babaMar",
-    author_email="author@example.com",
-    description="Manage sound and images",
+    version="0.1.0",
+    author="TSAK",
+    author_email="tsakians@gmail.com",
+    description="CLI tools for audio-signal manipulations",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/babaMar/soundfactory",
-    # packages=find_packages(),
+    license='MIT',
+    include_package_data=True,
     python_requires='>=3.5',
     install_requires=requirements,
     entry_points={
         'console_scripts': [
             'soundfactory = soundfactory:main',
         ]
-    }
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Artistic Software',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
