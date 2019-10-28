653cce3e0d4d0c9198f144084559c98ca355ce47
Stefano Messina <stefano.messina@customergauge.com>
2019-10-28 15:29:04 +0100

    Remove unnecessary where clause

--- soundfactory/settings/signal.py
+++ soundfactory/settings/signal.py
@@ -9,5 +9,5 @@ B_N_COEFF_MAP = {
     'sine': lambda x: where(x == 1, 1., 0),
     'sawtooth': lambda x: - 2./(pi * x),
     'square': lambda x: where(x % 2 == 1, 4./(pi * x), 0),
-    'triangle': lambda x: where(x % 2 == 1, real_part_or_zero_otherwise(alternate_minus_odd(x)) * (8./(pi * x)**2), 0)
+    'triangle': lambda x: real_part_or_zero_otherwise(alternate_minus_odd(x)) * (8./(pi * x)**2)
 }
