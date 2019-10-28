73e0f581156d36908d90b925bfd92926d2f27d25
Stefano Messina <stefano.messina@customergauge.com>
2019-10-28 13:30:08 +0100

    Handle runtime warning from numpy

--- soundfactory/settings/signal.py
+++ soundfactory/settings/signal.py
@@ -1,15 +1,13 @@
-from numpy import pi, where
+from numpy import pi, where, complex, isreal
 
+alternate_minus_odd = lambda x: (-1.)**((x.astype(complex) - 1)/2)
 
-alternate_minus_odd = lambda x: (-1.)**((x - 1)/2)
-
-
-alternate_minus = lambda x: (-1.)**(x + 1)
+real_part_or_zero_otherwise = lambda x: where(isreal(x), x, 0).real
 
 
 B_N_COEFF_MAP = {
     'sine': lambda x: where(x == 1, 1., 0),
     'sawtooth': lambda x: - 2./(pi * x),
-    'square': lambda x: where(x % 2 == 1, 4./(pi * x), 0),  # if x % 2 != 0 else 0,
-    'triangle': lambda x: where(x % 2 == 1, alternate_minus_odd(x) * (8./(pi * x)**2), 0)  # if x % 2 != 0 else 0,
+    'square': lambda x: where(x % 2 == 1, 4./(pi * x), 0),
+    'triangle': lambda x: where(x % 2 == 1, real_part_or_zero_otherwise(alternate_minus_odd(x)) * (8./(pi * x)**2), 0)
 }
