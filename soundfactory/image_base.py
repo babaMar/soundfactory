3d51403c0cc8292237f6941d3980a542c98f0e90
labaffa <dellarovere.lante@gmail.com>
2019-12-19 15:55:23 +0100

    Add SVD class and make Channel.svd a property

--- soundfactory/image_base.py
+++ soundfactory/image_base.py
@@ -3,18 +3,23 @@ from scipy import linalg
 import numpy as np
 
 
+class SVD:
+    def __init__(self, data):
+        M, N = np.asarray(data).shape
+        self.U, self.s, self.V = linalg.svd(data)
+        self.diagsvd = linalg.diagsvd(self.s, M, N)
+
+
 class Channel:
     def __init__(self, data, name):
         self.data = np.asarray(data)
         self.name = name
-        self._svd()
-        
-    def _svd(self):
-        M, N = self.data.shape
-        self.U, self.s, self.V = linalg.svd(self.data)
-        self.diagsvd = linalg.diagsvd(self.s, M, N)
 
+    @property
+    def svd(self):
+        return SVD(self.data)
 
+    
 class SoundImage:
     def __init__(self, input_file, **kw):
         self.image = Image.open(input_file)
