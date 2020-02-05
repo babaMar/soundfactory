01fd98e886d3c3647c37137c47240ec231c84287
Stefano Messina <essemexes@gmail.com>
2020-02-05 13:29:31 +0100

    Set scaled signal in constructor

--- soundfactory/signal_builder.py
+++ soundfactory/signal_builder.py
@@ -69,7 +69,7 @@ class SignalBuilder:
         )
         self.a0 = 0
         self.signal = self.build_signal()
-        self.scaled_signal = self.signal
+        self.scaled_signal = self.signal / np.max(np.abs(self.signal), axis=0)
 
     def get_time_space(self):
         return self.time_space
@@ -111,5 +111,4 @@ class SignalBuilder:
         return signal
 
     def export(self, path, bit_depth=16):
-        self.scaled_signal /= np.max(np.abs(self.signal), axis=0)
         write(self.scaled_signal, path, samplerate=self.samplerate, bit_depth=bit_depth)
